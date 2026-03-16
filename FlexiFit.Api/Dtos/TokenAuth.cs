1fec29d7b83f089117860cdff45144408fdef7c9
Dang CJ <53930227+CjConvento@users.noreply.github.com>
2026-03-17 01:10:04 +0800

    13-17-26 new latest commit completed flexifit api

---
+++ FlexiFit.Api/Dtos/TokenAuth.cs
@@ -0,0 +1,8 @@
+﻿namespace FlexiFit.Api.Dtos
+{
+    public class TokenAuth
+    {
+        public string Email { get; set; }
+        public string FirebaseUid { get; set; } // Dito papasok ang UID galing Swagger
+    }
+}
