6fe1e6de646c144c7922c6cfd8f653bcb3a09bf0
Dang CJ <53930227+CjConvento@users.noreply.github.com>
2026-03-24 14:22:06 +0800

    Revert "working workout and nutrition planner"
    This reverts commit 70aa64248cebc876dc6cc0c26b01a75df2c11619.

--- FlexiFit.Api/Dtos/UserManagementResponse.cs
+++ FlexiFit.Api/Dtos/UserManagementResponse.cs
@@ -2,22 +2,5 @@
 {
     public class UserManagementResponse
     {
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Gender { get; set; }
-        public int Age { get; set; }
-        public double HeightCm { get; set; }
-        public double WeightKg { get; set; }
-        public double Bmi { get; set; }
-        public string BmiCategory { get; set; }
-        public string GoalSubtitle { get; set; }
-        public int CompletedSessions { get; set; }  // map to totalSessions in Android
-        public int TotalProgramSessions { get; set; }
-        public int AchievementCount { get; set; }
-        public List<string> UnlockedBadgeKeys { get; set; }
-        public int DailyCalorieTarget { get; set; }
-        public double ProteinG { get; set; }
-        public double CarbsG { get; set; }
-        public double FatsG { get; set; }
     }
 }
